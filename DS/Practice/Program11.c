1994a4a0350ba1432b0e19990a3ebff38d873b93
AbhinandanSViroje <abhisviroje@gmail.com>
2026-07-05 23:54:55 +0530

    ds practice

---
+++ DS/Practice/Program11.c
@@ -0,0 +1,57 @@
+/*
+Q) Why do we use temp = temp->next; in a linked list? 
+
+temp = temp->next; moves the pointer to the next node in the linked list. 
+Unlike i = i + 1, linked list nodes are not stored in contiguous memory, so we use the next pointer -> to reach the next node instead of adding 1.
+
+*/
+
+#include<stdio.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+
+int main()
+{
+    PNODE head = NULL;
+    PNODE temp = NULL;
+
+    NODE obj1,obj2,obj3;
+
+    head = &obj1;
+    
+     obj1.data = 11;
+    obj1.next = &obj2;
+
+    obj2.data = 21;
+    obj2.next = &obj3;
+
+    obj3.data = 51;
+    obj3.next = NULL;
+
+    temp = head;                                // 100 (Imaginary Address)
+
+   // sequence 
+
+   printf("%d\n",temp -> data);                 // 11 (the data that is inside the 100 address)
+   
+   temp = temp -> next;                         //200
+   printf("%d\n",temp -> data);                 //21 
+
+   temp = temp -> next;                         //300
+   printf("%d\n",temp -> data);                 // 51
+
+   temp = temp -> next;                         //NULL
+
+
+
+    return 0;
+
+}
---
+++ DS/Practice/Program9.c
@@ -0,0 +1,36 @@
+// used typedef for pointer and structure
+
+#include<stdio.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+
+int main()
+{
+    PNODE head = NULL;
+    NODE obj1,obj2,obj3;
+
+    head = &obj1;
+
+    obj1.data = 11;
+    obj1.next = &obj2;      // we are storing Address of obj2 into obj1 next pointer
+
+    obj2.data = 21;
+    obj2.next = &obj3;     // we are storing Address of obj3 into obj2 next pointer
+
+    obj3.data = 51;
+    obj3.next = NULL;
+
+    printf("%d\n",head->data);
+    printf("%d\n",head -> next -> data);
+    printf("%d\n",head -> next -> next -> data);
+
+    return 0;
+}
